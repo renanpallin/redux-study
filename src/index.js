00359c2c2743cf86697b8c479a67a36c7d38b197
Renan Pallin <renanpallin@gmail.com>
2017-10-09 14:49:29 -0300

    todoReducer, auxiliar para ser usuado com o todosReducer

--- src/index.js
+++ src/index.js
@@ -5,16 +5,38 @@ import './index.css';
 
 import * as Redux from 'redux';
 
+const todoReducer = (state = {}, action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return {
+				id: action.id,
+				text: action.text,
+				done: false
+			};
+		case 'TOOGLE_TODO':
+			if (state.id !== action.todo.id)
+				return state;
+			return {
+				...state,
+				done: !state.done
+			}
+		default:
+			return state;
+	}
+}
+
 const todosReducer = (state = [], action) => {
 	switch (action.type) {
 		case 'ADD_TODO':
-			return [...state, { id: action.id, text: action.text, done: false }];
+			return [...state, todoReducer(null, action)];
 		case 'TOOGLE_TODO':
-			return state.map(todo => {
-				if (todo.id !== action.todo.id)
-					return todo;
-				return {...todo, done: !action.todo.done}
-			})
+			return state.map(todo => todoReducer(todo, action))
+			// return state.map(todo => {
+			// 	if (todo.id !== action.todo.id)
+			// 		return todo;
+			// 	return {...todo, done: !action.todo.done}
+			// })
+			
 		// case 'REMOVE_TODO':
 		// 	const our = state.findIndex(t => t.id === todo.id);
 		// 	return [
